653920b44ecdd1162111cd8f63a097129532e16b
i-am-robin <developerrobin@gmail.com>
2023-10-21 09:45:23 +0600

    first commit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,84 +1,79 @@
-import React from "react";
+"use client";
+import { useEffect, useRef, useState } from "react";
+// import Peer from "peerjs";
 
-function HomePage() {
-  let peer = new Peer();
+function App() {
+  const [peerId, setPeerId] = useState("");
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+  const remoteVideoRef = useRef(null);
+  const currentUserVideoRef = useRef(null);
+  const peerInstance = useRef(null);
 
-  function inti(userId) {
-    peer = new Peer(userId);
-    peer.on("open", (id) => {
-      console.log(id + "connected");
-    });
+  useEffect(() => {
+    import("peerjs").then(({ default: Peer }) => {
+      const peer = new Peer();
 
-    listenToCall();
-  }
-
-  function listenToCall() {
-    peer.on("call", (call) => {
-      navigator.mediaDevices
-        .getUserMedia({
-          video: true,
-          audio: true,
-        })
-        .then((stream) => {
-          myStream = stream;
-          addLocalVideo(stream);
-          call.answer(stream);
-          call.on("stream", (remoteStream) => {
-            if (!peerList.includes(call.peer)) {
-              addRemoteVideo(remoteStream);
-              peerList.push(call.peer);
-            }
-          });
-        })
-        .catch((err) => {
-          console.log("unable to connect because " + err);
-        });
-    });
-  }
-
-  //this function will be called when we try to make a call
-  function makeCall(receiverId) {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then((stream) => {
-        myStream = stream;
-        addLocalVideo(stream);
-        let call = peer.call(receiverId, stream);
-        call.on("stream", (remoteStream) => {
-          if (!peerList.includes(call.peer)) {
-            addRemoteVideo(remoteStream);
-            peerList.push(call.peer);
-          }
-        });
-      })
-      .catch((err) => {
-        console.log("unable to connect because " + err);
+      peer.on("open", (id) => {
+        setPeerId(id);
       });
-  }
 
-  //this function will add local stream to video pannel
-  function addLocalVideo(stream) {
-    let video = document.createElement("video");
-    video.srcObject = stream;
-    video.classList.add("video");
-    video.muted = true; // local video need to be mute because of noise issue
-    video.play();
-    document.getElementById("localVideo").append(video);
-  }
+      peer.on("call", (call) => {
+        var getUserMedia =
+          navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia;
 
-  //this function will add remote stream to video pannel
-  function addRemoteVideo(stream) {
-    let video = document.createElement("video");
-    video.srcObject = stream;
-    video.classList.add("video");
-    video.play();
-    document.getElementById("remoteVideo").append(video);
-  }
+        getUserMedia({ video: true, audio: true }, (mediaStream) => {
+          currentUserVideoRef.current.srcObject = mediaStream;
+          currentUserVideoRef.current.play();
+          call.answer(mediaStream);
+          call.on("stream", function (remoteStream) {
+            remoteVideoRef.current.srcObject = remoteStream;
+            remoteVideoRef.current.play();
+          });
+        });
+      });
 
-  return <div>HomePage</div>;
+      peerInstance.current = peer;
+    });
+  }, []);
+
+  const call = (remotePeerId) => {
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    getUserMedia({ video: true, audio: true }, (mediaStream) => {
+      currentUserVideoRef.current.srcObject = mediaStream;
+      currentUserVideoRef.current.play();
+
+      const call = peerInstance.current.call(remotePeerId, mediaStream);
+
+      call.on("stream", (remoteStream) => {
+        remoteVideoRef.current.srcObject = remoteStream;
+        remoteVideoRef.current.play();
+      });
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Current user id is {peerId}</h1>
+      <input
+        type="text"
+        value={remotePeerIdValue}
+        onChange={(e) => setRemotePeerIdValue(e.target.value)}
+      />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button>
+      <div>
+        <video ref={currentUserVideoRef} />
+      </div>
+      <div>
+        <video ref={remoteVideoRef} />
+      </div>
+    </div>
+  );
 }
 
-export default HomePage;
+export default App;
