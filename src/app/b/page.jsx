882e70e82ad7260c08f5b3179a68ef21a89222c6
i-am-robin <developerrobin@gmail.com>
2023-10-24 20:28:01 +0600

    firsst

--- src/app/b/page.jsx
+++ src/app/b/page.jsx
@@ -1,121 +1,75 @@
 "use client";
-import { socket } from "@/lib/socket.io/socket";
-import React, { useEffect, useState, useRef } from "react";
-import Peer from "simple-peer";
+import { useEffect, useRef, useState } from "react";
+import Peer from "peerjs";
 
 function App() {
-  const [yourID, setYourID] = useState("");
-  const [users, setUsers] = useState({});
-  const [stream, setStream] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
-
-  const userVideo = useRef();
-  const partnerVideo = useRef();
-
-  const getStream = async () => {
-    const videoStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-
-    userVideo.current.srcObject = videoStream;
-
-    setStream(videoStream);
-
-    return videoStream;
-  };
+  const [peerId, setPeerId] = useState("");
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+  const remoteVideoRef = useRef(null);
+  const currentUserVideoRef = useRef(null);
+  const peerInstance = useRef(null);
 
   useEffect(() => {
-    getStream();
+    const peer = new Peer();
 
-    socket.on("yourID", (id) => {
-      setYourID(id);
+    peer.on("open", (id) => {
+      setPeerId(id);
     });
 
-    socket.on("allUsers", (users) => {
-      setUsers(users);
-    });
+    peer.on("call", (call) => {
+      var getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
 
-    socket.on("hey", (data) => {
-      setReceivingCall(true);
-      setCaller(data.from);
-      setCallerSignal(data.signal);
-    });
-  }, []);
-
-  function callPeer(id) {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-    });
-
-    peer.on("signal", (data) => {
-      socket.emit("callUser", {
-        userToCall: id,
-        signalData: data,
-        from: yourID,
+      getUserMedia({ video: true, audio: true }, (mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream;
+        currentUserVideoRef.current.play();
+        call.answer(mediaStream);
+        call.on("stream", function (remoteStream) {
+          remoteVideoRef.current.srcObject = remoteStream;
+          remoteVideoRef.current.play();
+        });
       });
     });
 
-    peer.on("stream", (stream) => {
-      if (partnerVideo.current) {
-        partnerVideo.current.srcObject = stream;
-      }
-    });
+    peerInstance.current = peer;
+  }, []);
 
-    socket.on("callAccepted", (signal) => {
-      setCallAccepted(true);
-      peer.signal(signal);
-    });
-  }
+  const call = (remotePeerId) => {
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
 
-  function acceptCall() {
-    setCallAccepted(true);
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on("signal", (data) => {
-      socket.emit("acceptCall", { signal: data, to: caller });
-    });
+    getUserMedia({ video: true, audio: true }, (mediaStream) => {
+      currentUserVideoRef.current.srcObject = mediaStream;
+      currentUserVideoRef.current.play();
 
-    peer.on("stream", (stream) => {
-      partnerVideo.current.srcObject = stream;
-    });
+      const call = peerInstance.current.call(remotePeerId, mediaStream);
 
-    peer.signal(callerSignal);
-  }
+      call.on("stream", (remoteStream) => {
+        remoteVideoRef.current.srcObject = remoteStream;
+        remoteVideoRef.current.play();
+      });
+    });
+  };
 
   return (
-    <div>
-      <p>YOur id: {yourID}</p>
+    <div className="App">
+      <h1>Current user id is {peerId}</h1>
+      <input
+        type="text"
+        value={remotePeerIdValue}
+        onChange={(e) => setRemotePeerIdValue(e.target.value)}
+      />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button>
       <div>
-        <video autoPlay ref={userVideo}></video>
-        <video autoPlay ref={partnerVideo}></video>
+        <video muted ref={currentUserVideoRef} />
       </div>
       <div>
-        {Object.keys(users).map((key, i) => {
-          if (key === yourID) {
-            return <p>{"you: " + yourID}</p>;
-          }
-          return (
-            <button key={i} className="block" onClick={() => callPeer(key)}>
-              Call {key}
-            </button>
-          );
-        })}
+        <video ref={remoteVideoRef} />
       </div>
-      {receivingCall && (
-        <div>
-          <h1>{caller} is calling you</h1>
-          <button onClick={acceptCall}>Accept</button>
-        </div>
-      )}
     </div>
   );
 }
