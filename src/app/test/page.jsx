5b0a365add5f2ca963c3ea0665580662861a21e2
i-am-robin <developerrobin@gmail.com>
2023-10-27 19:01:14 +0600

    added s

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -2,11 +2,20 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import Peer from "simple-peer";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
 
 function Chattest() {
   const [videoStream, setVideoStream] = useState();
   const [peerId, setPeerId] = useState();
   const [otherPeerId, setOtherPeerId] = useState();
+  const [socketId, setSocketId] = useState();
+  const [geatingCall, setGeatingCall] = useState(false);
+
+  const [caller, setCaller] = useState();
+
+  const [users, setUsers] = useState([]);
 
   const userVideoRef = useRef();
   const callerVideoRef = useRef();
@@ -14,20 +23,43 @@ function Chattest() {
   const peerRef = useRef();
 
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then((stream) => {
-        setVideoStream(stream);
-        userVideoRef.current.srcObject = stream;
+    try {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: true,
+        })
+        .then((stream) => {
+          setVideoStream(stream);
+          userVideoRef.current.srcObject = stream;
+        });
+      4;
+
+      socket.on("connect", () => {
+        console.log("hi");
+        socket.on("me", (id) => {
+          console.log(id);
+          setSocketId(id);
+        });
+      });
+      4;
+
+      socket.on("allUsers", (users) => {
+        setUsers(users);
+        console.log(users);
       });
 
-    return () => {};
+      socket.on("hey", (data) => {
+        setGeatingCall(true);
+        setCaller(data.from);
+        setOtherPeerId(data.signal);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
-  function makeNewCall() {
+  function callPeer(id) {
     const peer = new Peer({
       initiator: true,
       trickle: false,
@@ -35,18 +67,20 @@ function Chattest() {
     });
 
     peer.on("signal", (data) => {
-      setPeerId(JSON.stringify(data));
+      socket.emit("callUser", {
+        signalData: data,
+        userToCall: id,
+        from: socketId,
+      });
     });
 
     peer.on("stream", (data) => {
       callerVideoRef.current.srcObject = data;
     });
 
-    peerRef.current = peer;
-  }
-
-  function connectAns() {
-    peerRef.current.signal(otherPeerId);
+    socket.on("callAccepted", (signal) => {
+      peer.signal(signal);
+    });
   }
 
   function ansPeer() {
@@ -56,7 +90,7 @@ function Chattest() {
       stream: videoStream,
     });
     peer.on("signal", (data) => {
-      setPeerId(JSON.stringify(data));
+      socket.emit("acceptCall", { signal: data, to: caller });
     });
 
     peer.on("stream", (data) => {
@@ -68,24 +102,19 @@ function Chattest() {
 
   return (
     <div>
-      <textarea
-        cols="30"
-        rows="10"
-        value={peerId}
-        className="border-red-prymary text-black-dark border-2 block m-2 rounded-md px-2"></textarea>
-      <button onClick={makeNewCall}>Make call</button>
-      <textarea
-        cols="30"
-        rows="10"
-        value={otherPeerId}
-        onChange={(e) => setOtherPeerId(e.target.value)}
-        className="border-red-prymary text-black-dark border-2 block m-2 rounded-md px-2"></textarea>
-      <button onClick={ansPeer}>AnsCall</button>
+      <p>Socket id : {socketId}</p>
+      {geatingCall ? <button onClick={ansPeer}>Answe call</button> : null}
       <br />
-      <button onClick={connectAns}>Conect for caller</button>
-      {/* a */}
       <video autoPlay muted ref={userVideoRef}></video>
       <video autoPlay ref={callerVideoRef}></video>
+
+      {users.map((user, i) => {
+        return (
+          <button className="block" onClick={() => callPeer(user)} key={i}>
+            call: {user}
+          </button>
+        );
+      })}
     </div>
   );
 }
