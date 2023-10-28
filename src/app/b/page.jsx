beb9dfdd351109d7906e38dd79bc8ffdb3719157
i-am-robin <developerrobin@gmail.com>
2023-10-28 19:06:41 +0600

    a

--- src/app/b/page.jsx
+++ src/app/b/page.jsx
@@ -1,25 +1,25 @@
-"use client";
+// "use client";
 
-import React, { useEffect } from "react";
-import { Peer } from "peerjs";
+// import React, { useEffect } from "react";
+// import { Peer } from "peerjs";
 
-//
+// //
 
-function Page() {
-  useEffect(() => {
-    const peer = new Peer();
-
-    peer.on("open", (id) => {
-      console.log(id);
-    });
-    return () => {};
-  }, []);
-  return <div>Page</div>;
-}
-
-export default Page;
-
-// import("peerjs").then(({ default: Peer }) => {
-//     // normal synchronous code
+// function Page() {
+//   useEffect(() => {
 //     const peer = new Peer();
-//   });
+
+//     peer.on("open", (id) => {
+//       console.log(id);
+//     });
+//     return () => {};
+//   }, []);
+//   return <div>Page</div>;
+// }
+
+// export default Page;
+
+// // import("peerjs").then(({ default: Peer }) => {
+// //     // normal synchronous code
+// //     const peer = new Peer();
+// //   });
